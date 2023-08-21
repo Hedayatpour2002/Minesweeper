b49e8ea8d06f1cf10b9196436d9a4e59a537a77c
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2023-08-21 14:35:50 +0330

    Improve readability and understanding of code
    * Added explanatory comments to explain the code's behavior
    * Cleaned up the code to improve readability
    * Renamed mineSweeper__map to mineSweeper__board

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const $ = document;
 
 const mineSweeper = $.querySelector(".mineSweeper");
-const mineSweeperMap = $.querySelector(".mineSweeper__map");
+const mineSweeperBoard = $.querySelector(".mineSweeper__board");
 
 let row = 9;
 let column = 9;
@@ -11,6 +11,7 @@ let minePosition = [];
 
 renderSquare();
 
+/* This function creates a square button for each row and column. */
 function renderSquare() {
   let squareWidth = 100 / column;
   for (let i = 0; i < row; i++) {
@@ -23,13 +24,14 @@ function renderSquare() {
       square.setAttribute("data-number", `${i * column + j}`);
 
       square.style.width = `${squareWidth}%`;
-      mineSweeperMap.appendChild(square);
+      mineSweeperBoard.appendChild(square);
       square.style.height = getComputedStyle(square).getPropertyValue("width");
     }
   }
   randomMinePosition();
 }
 
+/* This function randomly generates the positions of the mines. */
 function randomMinePosition() {
   for (let i = 0; i < mineCount; i++) {
     let newPosition = {
@@ -55,6 +57,7 @@ function randomMinePosition() {
   }
 }
 
+/* This function opens a square and its adjacent squares if there are no mines around it. */
 function processSquare(square) {
   let stack = [square];
   let processedSquares = [];
@@ -108,6 +111,13 @@ function processSquare(square) {
       }
     }
 
+    /*
+      Iterate through the adjacent square positions and count the number of mines.
+
+      If the number of mines is 0, the adjacent squares are added to the stack.
+
+      If the number of mines is not 0, the current square is updated with the number of mines.
+    */
     aroundSquarePositions.forEach((aroundSquarePosition) => {
       minePosition.forEach((mine) => {
         if (mine.number === aroundSquarePosition) aroundMineCount++;
@@ -129,80 +139,11 @@ function processSquare(square) {
   }
 }
 
-/*
-
-function processSquare(square) {
-  square.classList.add("open");
-
-  let mainSquarePosition = Number(square.dataset.number);
-  let mainSquareRow = square.dataset.row;
-  let mainSquareColumn = square.dataset.column;
-  let aroundMineCount = 0;
-  let aroundSquares = [];
-  let aroundSquarePositions = [];
-
-  // Get the adjacent square positions.
-  for (
-    let i = mainSquarePosition - column - 1;
-    i <= mainSquarePosition + column + 1;
-    i += column
-  ) {
-    if (mainSquareRow == 0 && mainSquareColumn == 0 && i < 0) {
-      i = 0;
-    }
-    if (i < 0) {
-      continue;
-    }
-
-    for (let j = 0; j < 3; j++) {
-      if (mainSquareRow == 0 && mainSquareColumn == 0 && j === 0) {
-        j = 0;
-      } else if (mainSquareColumn == 0 && j === 0) {
-        j = 1;
-      }
-
-      if (mainSquareRow == 0 && mainSquareColumn == 0 && j === 2) {
-        break;
-      }
-      if (mainSquareColumn == column - 1 && j === 2) {
-        break;
-      }
-      if (i + j >= row * column) {
-        continue;
-      }
-
-      aroundSquarePositions.push(i + j);
-    }
-  }
-
-  aroundSquarePositions.forEach((aroundSquarePosition) => {
-    minePosition.forEach((mine) => {
-      if (mine.number === aroundSquarePosition) aroundMineCount++;
-    });
-    let aroundSquare = selectMineWithNumber(aroundSquarePosition);
-    if (
-      !aroundSquare.classList.contains("open") ||
-      !aroundSquare.classList.contains("flag") ||
-      !aroundSquare.classList.contains("showMine")
-    )
-      aroundSquares.push(aroundSquare);
-  });
-
-  if (!aroundMineCount) {
-    console.log(aroundSquares);
-    aroundSquares.forEach((aroundSquare) => {
-      processSquare(aroundSquare);
-    });
-  } else {
-    square.innerHTML = aroundMineCount;
-  }
-}
-*/
-
 function endgame() {
   alert("Game ended");
 }
 
+/* This function shows all the mines on the board. */
 function showMine() {
   minePosition.forEach((position) => {
     let mine = selectMineWithNumber(position.number);
@@ -210,20 +151,36 @@ function showMine() {
   });
 }
 
+// Currently unused
 function selectMine(position) {
   return document.querySelector(
     `[data-number = "${position.row}${position.column}"]`
   );
 }
+
+/* This function returns the mine button with the specified number. */
 function selectMineWithNumber(number) {
   return document.querySelector(`[data-number = "${number}"]`);
 }
 
-mineSweeperMap.addEventListener("click", (e) => {
+mineSweeperBoard.addEventListener("click", (e) => {
+  /*
+    This function is called when the player clicks on a square.
+
+    The function takes the event object as an argument and opens the square if it does not contain a mine.
+
+    If the square contains a mine, the game ends.
+
+    If the square is already open or flagged, the function does nothing.
+  */
+
   let target = e.target;
   if (target.classList.contains("square")) {
     let number = Number(target.dataset.number);
-
+    /*
+      Check if the square contains a mine.
+      If it does, end the game.
+    */
     if (
       minePosition.some((mine) => {
         return mine.number === number;
@@ -235,23 +192,42 @@ mineSweeperMap.addEventListener("click", (e) => {
     }
 
     let square = selectMineWithNumber(number);
-
+    /*
+      Check if the square is already open or flagged.
+      If it is, do nothing.
+    */
     if (
       square.classList.contains("open") ||
       square.classList.contains("flag") ||
       square.classList.contains("showMine")
     ) {
     } else {
+      /*
+        Call the processSquare function with the square clicked
+     */
       processSquare(square);
     }
   }
 });
 
-mineSweeperMap.addEventListener("contextmenu", (e) => {
+mineSweeperBoard.addEventListener("contextmenu", (e) => {
+  /*
+    This function is called when the player right-clicks on a square.
+
+    The function takes the square button as an argument and toggles its flag status.
+
+    If the square is already flagged, the flag is removed.
+
+    If the square is not flagged, a flag is added to it.
+  */
   e.preventDefault();
   let target = e.target;
   if (target.classList.contains("square")) {
     let square = selectMineWithNumber(Number(target.dataset.number));
+    /*
+      Check if the square is already open or shows a mine.
+      If it is, do nothing.
+    */
     if (
       square.classList.contains("open") ||
       square.classList.contains("showMine")
